7811c8123d80f7d213febe1ed1f720ebdf5bc7c8
Sh3dow-ware <u2486896@rts-365.dk>
2025-09-01 09:10:09 +0200

    Added Start Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import {ReactNode} from "react";
+
+
+
+
+export const metadata: Metadata = {
+  title: "Landrup Dans",
+  description: "Landrup Dans",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
