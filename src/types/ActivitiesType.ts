49f102bd4aa0a57752c54e02f0179ad4d4bdc96f
Sh3dow-ware <u2486896@rts-365.dk>
2025-09-01 14:07:11 +0200

    Added ActivitiesType.ts for type-safety

---
+++ src/types/ActivitiesType.ts
@@ -0,0 +1,21 @@
+export interface ActivitiesProps {
+  id?: number
+  name: string;
+  minAge: number;
+  maxAge: number;
+  description: string;
+  weekday: string
+  time: string
+  asset: AssetProps
+}
+
+export type AssetProps = {
+  id?: number;
+  url: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type GetSpecificActivityType = {
+  activity: ActivitiesProps
+}
