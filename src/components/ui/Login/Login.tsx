1e8e8c20e422c1b0865c972033f6137d66ff14d0
Sh3dow-ware <u2486896@rts-365.dk>
2025-09-03 15:46:34 +0200

    Added Login

---
+++ src/components/ui/Login/Login.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import {useActionState} from "react";
+import {loginAction} from "@/app/action/login";
+import "@/styles/components/login/login.sass"
+
+export const Login = () => {
+
+  const [formState, formAction, isPending] = useActionState(loginAction, {})
+
+  return (
+      <>
+        <div className={"login-wrapper"}>
+          <h2 className={"login-wrapper__title"}>Log Ind</h2>
+          <form className={"login-form"} action={formAction}>
+            <label className={"login-form__label"} htmlFor={"username"}>
+              <input className={"login-form__input"} type={"text"} name={"username"} placeholder={"brugernavn"}/>
+              <p>{!formState.success ? formState.properties?.username?.errors : null}</p>
+            </label>
+            <label className={"login-form__label"} htmlFor={"password"}>
+              <input className={"login-form__input"} type={"password"} name={"password"} placeholder={"adgangskode"}/>
+              <p>{!formState.success ? formState.properties?.password?.errors : null}</p>
+            </label>
+            <button type={"submit"} className={"login-form__submit"}>Log Ind</button>
+          </form>
+        </div>
+      </>
+  );
+};
